7d24068c632d99edbb09ed1c19d59ed9ceba7a2b
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2023-03-29 20:36:21 -0300

    sidebar responsivo

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const btnMobile = document.querySelector('.btn-mobile');
+const btnMobileClose = document.getElementById('btn-mobile-close');
+
+btnMobile.addEventListener('click', mostrarMenuMobile);
+btnMobileClose.addEventListener('click', removendoMenuMobile);
+
+function mostrarMenuMobile() {
+    const menuMobile = document.querySelector('.menu-mobile');
+
+    if (menuMobile.classList.value === 'menu-mobile') {
+        menuMobile.classList.value = 'menu-mobile-active';
+    }
+}
+
+function removendoMenuMobile() {
+    const menuMobile = document.querySelector('.menu-mobile-active');
+
+    if (menuMobile.classList.value == 'menu-mobile.active');
+    menuMobile.classList.value = 'menu-mobile';
+}
